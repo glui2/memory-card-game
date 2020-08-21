0ffd3fddad3c63c739f74d560b968b0471f6440f
Gavin <gavin.lui@gmail.com>
2020-08-22 00:02:35 +1000

    moved card styling to css, centred card components in svg

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import "./Card.css";
 
 const Card = () => {
   return (
-    <svg width="200" height="300">
+    <svg width="200" height="200" display="block" className="card-svg">
       <defs>
         <linearGradient id="card-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
           <stop offset="0%" stopColor="rgb(255,255,255)" stopOpacity="1" />
@@ -14,10 +14,7 @@ const Card = () => {
         </filter>
       </defs>
       <rect
-        x="10"
-        y="10"
-        width="100"
-        height="150"
+        className="card-shadow"
         stroke="grey"
         strokeWidth="2"
         rx="10"
@@ -28,7 +25,7 @@ const Card = () => {
       <rect
         className="card"
         stroke="grey"
-        strokeWidth="2"
+        strokeWidth="1"
         rx="10"
         ry="10"
         fill="url(#card-gradient)"
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import { Card } from "../components/Card";
+import Grid, { GridSpacing } from "@material-ui/core/Grid";
 
 const MainPage = () => {
   return (
     <div>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+      <Grid container spacing={3}>
+        <Grid item>
+          <Card></Card>
+        </Grid>
+        <Grid item>
+          <Card></Card>
+        </Grid>
+        <Grid item>
+          <Card></Card>
+        </Grid>
+        <Grid item>
+          <Card></Card>
+        </Grid>
+        <Grid item>
+          <Card></Card>
+        </Grid>
+      </Grid>
     </div>
   );
 };
