c71738f4071101f0868cabb3a7f767f65dbdf692
Gavin <gavin.lui@gmail.com>
2020-08-27 23:10:07 +1000

    added state for whether card is flipped face up or not, added color gradients for both

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,28 @@ import React, { useState } from "react";
 import "./Card.css";
 
 const Card = () => {
+  const [isCardOpenedState, setIsCardOpenedState] = useState(false);
+
+  const flipCard = () => {
+    if (isCardOpenedState) {
+      setIsCardOpenedState(false);
+    } else {
+      setIsCardOpenedState(true);
+    }
+  };
+
   return (
     <svg width="200" height="200" display="block" className="card-svg">
       <defs>
-        <linearGradient id="card-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+        <linearGradient id="white-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
           <stop offset="0%" stopColor="rgb(255,255,255)" stopOpacity="1" />
           <stop offset="100%" stopColor="rgb(200,200,200)" stopOpacity="1" />
         </linearGradient>
+        <linearGradient id="black-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" stopColor="rgb(125,125,125)" stopOpacity="1" />
+          <stop offset="100%" stopColor="rgb(0,0,0)" stopOpacity="1" />
+        </linearGradient>
+
         <filter id="shadow">
           <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
         </filter>
@@ -23,12 +38,12 @@ const Card = () => {
         filter="url(#shadow)"
       />
       <rect
-        className="card"
-        stroke="grey"
-        strokeWidth="1"
+        className={isCardOpenedState ? "card showWhite" : "card showBlack"}
+        stroke="yellow"
+        strokeWidth="3"
         rx="10"
         ry="10"
-        fill="url(#card-gradient)"
+        onClick={flipCard}
       />
     </svg>
   );
