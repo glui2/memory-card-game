a5dc12bb1a21d8ce9eb27d62276d9b225da9413e
Gavin <gavin.lui@gmail.com>
2020-09-30 00:14:04 +1000

    fix bug that opened two cards at once

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -9,58 +9,85 @@ import { wait } from "@testing-library/react";
 const MainPage = () => {
   const [cards, setCards] = useState([]); // store card values in array of tupies with structure index: {value: int, cardState: open/closed}
   const [newTurn, setNewTurn] = useState(true); // store whether is a new turn (true, picking first card) or same turn (false, picking 2nd card)
-  const [firstCard, setFirstCard] = useState({
-    index: null,
-    value: null,
-  }); // store value of first card
+  const [firstCard, setFirstCard] = useState({}); // store value of first card
   const [score, setScore] = useState(0);
+
+  // useEffect(() => {
+  //   console.log({ firstCard });
+  // }, [firstCard]);
 
   const openCard = (index) => {
     console.log("Card index " + index + " has been clicked");
-    const currentCards = [...cards];
+    // const currentCards = [...cards];
     const selectedCard = cards[index];
+    console.log({ selectedCard });
 
     if (!selectedCard.isCardOpened) {
       selectedCard.isCardOpened = true;
+      console.log("Selected card is now open");
       // currentCards[index] = selectedCard;
-      setCards(currentCards);
+      // setCards(currentCards);
       console.log(cards);
-      console.log(selectedCard);
+      console.log({ selectedCard });
+
       if (!newTurn) {
         //compare value with firstCard
+        console.log("SECOND CARD SELECTED");
+        console.log({ firstCard });
         if (selectedCard.value === firstCard.value) {
           console.log("You've found a pair!");
           setScore(score + 1);
           setNewTurn(true);
           setFirstCard({ index: "", value: "" });
         } else {
-          currentCards[firstCard.index].isCardOpened = false;
-          currentCards[index].isCardOpened = false;
-          setCards(currentCards);
+          console.log("Try again!");
+          cards[firstCard.index].isCardOpened = false;
+          cards[index].isCardOpened = false;
+          console.log(
+            "flipping over cards with index: " + index + " " + firstCard.index
+          );
+          console.log(cards);
           setNewTurn(true);
           setFirstCard({ index: "", value: "" });
         }
       } else {
         //setFirstCard(this card's value)
+        console.log("FIRST CARD SELECTED");
+
         setFirstCard({ index: index, value: selectedCard.value });
         setNewTurn(false);
-        console.log(firstCard);
       }
     }
   };
 
   const generateCards = () => {
     setScore(0);
-    const randomArray = [];
+    var randomArray1 = [];
+    var randomArray2 = [];
 
     for (var i = 0; i < 9; i++) {
-      randomArray[i] = {
-        value: Math.floor(Math.random() * 10),
+      var randomNumber = Math.floor(Math.random() * 10);
+
+      randomArray1[i] = {
+        value: randomNumber,
+        isCardOpened: false,
+      };
+      randomArray2[i] = {
+        value: randomNumber,
         isCardOpened: false,
       };
     }
 
-    const pairsArray = randomArray.concat(randomArray);
+    console.log(randomArray1);
+
+    // randomArray2 = randomArray1.map((element) => {
+    //   return { ...element };
+    // });
+
+    console.log(randomArray2);
+
+    const pairsArray = randomArray1.concat(randomArray2);
+    console.log(pairsArray);
     const shuffledPairsArray = pairsArray.sort(() => {
       return Math.random() - 0.5;
     });
